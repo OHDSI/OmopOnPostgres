61d724888e3c50e71679fbb67a13e7034599dc9d
catalamarti <marticatalasabate@gmail.com>
2026-01-22 11:48:50 +0000

    add summary method
    fix #17

--- R/postgresSource.R
+++ R/postgresSource.R
@@ -247,6 +247,19 @@ readSourceTable.pq_cdm <- function(cdm, name) {
   readTable(src = cdm, name = name, type = "write")
 }
 
+#' @export
+summary.pq_cdm <- function(object, ...) {
+  list(
+    package = "OmopOnPostgres",
+    cdm_schema = attr(object, "cdm_schema"),
+    cdm_prefix = attr(object, "cdm_prefix"),
+    write_schema = attr(object, "write_schema"),
+    write_prefix = attr(object, "write_prefix"),
+    achilles_schema = attr(object, "achilles_schema"),
+    achilles_prefix = attr(object, "achilles_prefix")
+  )
+}
+
 computeTable <- function(src, type, name, sql, jobName) {
   # create sql
   name <- formatName(src = src, name = name, type = type)
