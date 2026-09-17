512f2b6b601f1c1fa5ca02304fd59d101e0f596d
Ed Burn <9583964+edward-burn@users.noreply.github.com>
2026-09-17 12:52:31 +0100

    withr order

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -2,10 +2,10 @@ for (drv in get_test_drivers()) {
 
   test_that(sprintf("create schema using %s", drv), {
   skip_on_cran()
+  con <- localPostgres(client = drv)
   withr::defer(DBI::dbDisconnect(conn = con))
   withr::defer(resetSchemas())
   withr::defer(dropSchema(con, "test_schema"))
-  con <- localPostgres(client = drv)
   dropSchema(con, "test_schema")
   expect_false(schemaExists(con = con, schema = "test_schema"))
   expect_no_error(createSchema(con = con, schema = "test_schema"))
